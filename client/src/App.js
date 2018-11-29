5cc23b89c4244e09398511e682bcbe295b654601
Anthony Agnone <ada60@pitt.edu>
2018-11-29 17:55:36 -0500

    added character setup for all stats

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,6 @@ export default function App() {
   return (
     <div>
       <h1>DnD 5e App</h1>
-      <h1>Classes</h1>
       <Test />
     </div>
   );
--- client/src/components/testData/Test.js
+++ client/src/components/testData/Test.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import PromiseHandler from '../api/PromiseHandler';
 import { withinView } from '../api/View';
 import ClassList from '../lists/ClassList';
+import RaceList from '../lists/RaceList';
 
 const cors = 'https://vschool-cors.herokuapp.com/?url=';
 
@@ -14,10 +15,19 @@ export default class Test extends Component {
       position: 0
     };
     this.getClassData = this.getClassData.bind(this);
+    this.getRaceData = this.getRaceData.bind(this);
   }
 
   getClassData() {
     const url = 'http://dnd5eapi.co/api/classes';
+
+    const apiQuery = `${cors}${url}`;
+
+    return axios.get(apiQuery).then(response => response.data.results);
+  }
+
+  getRaceData() {
+    const url = 'http://dnd5eapi.co/api/races';
 
     const apiQuery = `${cors}${url}`;
 
@@ -30,6 +40,10 @@ export default class Test extends Component {
         <PromiseHandler
           promise={this.getClassData}
           render={withinView(ClassList)}
+        />
+        <PromiseHandler
+          promise={this.getRaceData}
+          render={withinView(RaceList)}
         />
       </div>
     );
