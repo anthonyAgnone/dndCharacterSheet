d86b70b00d6ce3e7d57a005c6b5e717d9bc5302e
Anthony Agnone <ada60@pitt.edu>
2018-12-04 17:43:07 -0500

    added comment to stat display

--- client/src/components/statCalculations/StatDisplay.js
+++ client/src/components/statCalculations/StatDisplay.js
@@ -3,6 +3,7 @@ import React from 'react';
 const StatDisplay = ({statName, statValue}) => {
   return (
     <div>
+    {/* display the stats */}
       <h1>{statName}</h1>
       <p>{statValue}</p>
     </div>
