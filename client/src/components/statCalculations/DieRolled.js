ba51529e361ddc764e95eb0855bf9ba043091a93
Anthony Agnone <ada60@pitt.edu>
2018-12-06 18:15:16 -0500

    Used javascript and greensock animations to animate the dice

--- client/src/components/statCalculations/DieRolled.js
+++ client/src/components/statCalculations/DieRolled.js
@@ -1,241 +1,261 @@
-import React from 'react';
+import React, { Component, createRef } from 'react';
 import styled from 'styled-components';
 
-import one from './assets/one.png';
-import two from './assets/two.png';
-import three from './assets/three.png';
-import four from './assets/four.png';
-import five from './assets/five.png';
-import six from './assets/six.png';
-
-// const Die = styled.img`
-//   opacity: ${props => (props.isLowest ? '.1' : '1')};
-// `;
+import { TimelineMax, Power2 } from 'gsap';
 
 const Scene = styled.div`
   width: 80px;
   height: 80px;
-  border: 1px solid #ccc;
-  perspective: 400px;
+  transform-style: preserve-3d;
   margin-right: 40px;
-  .cube {
+  .side {
+    position: absolute;
     width: 80px;
     height: 80px;
-    position: relative;
-    transform-style: preserve-3d;
-    transform: translateZ(0deg) rotateY(0deg) rotateX(0deg);
-    animation-delay: ${props => props.delay}s;
-    background-color: #fff;
-    .face {
-      position: absolute;
-      width: 80px;
-      height: 80px;
-      background: #fff;
-      box-shadow: inset 0 0 40px #ccc;
-      border-radius: 4px;
-      overflow: hidden;
-      background-position: 50% 50%;
-      display: grid;
-      grid-template: repeat(3, calc(100% / 3)) / repeat(3, calc(100% / 3));
-      justify-items: center;
-      align-items: center;
-      .dot {
-        background: #444;
-        box-shadow: inset 3px 0 6px #000;
-        border: 2px solid black;
-        width: 16px;
-        height: 16px;
-        border-radius: 50%;
-      }
-      .dot:first-child {
-        grid-row: 2;
-        grid-column: 2;
-      }
-      .dot:nth-child(2) {
-        grid-row: 3;
-        grid-column: 3;
-      }
-      .dot:nth-child(3) {
-        grid-row: 1;
-        grid-column: 1;
-      }
-      .dot:nth-child(4) {
-        grid-row: 1;
-        grid-column: 3;
-      }
-      .dot:nth-child(5) {
-        grid-row: 2;
-        grid-column: 1;
-      }
-      .dot:nth-child(5) {
-        grid-row: 3;
-        grid-column: 1;
-      }
+    background: #fff;
+    box-shadow: inset 0 0 10px #ccc;
+    border-radius: 4px;
+    border: 2px solid #000;
+    display: grid;
+    grid-template:
+      repeat(3, calc(100% / 3)) /
+      repeat(3, calc(100% / 3));
+    justify-items: center;
+    align-items: center;
+    .dot {
+      width: 16px;
+      height: 16px;
+      border-radius: 23px;
+      background: #444;
+      box-shadow: inset 5px 0 10px #000;
     }
-    .face.back .dot:first-child {
+    .dot:first-child {
+      grid-row: 2;
+      grid-column: 2;
+    }
+    .dot:nth-child(2) {
+      grid-row: 3;
+      grid-column: 3;
+    }
+    .dot:nth-child(3) {
       grid-row: 1;
       grid-column: 1;
     }
-    .face.left .dot:first-child {
+    .dot:nth-child(4) {
+      grid-row: 1;
+      grid-column: 3;
+    }
+    .dot:nth-child(5) {
       grid-row: 3;
       grid-column: 1;
     }
-    .face.bottom {
-      .dot:first-child {
-        grid-row: 2;
-        grid-column: 1;
-      }
-      .dot:nth-child(2) {
-        grid-row: 3;
-        grid-column: 3;
-      }
-      .dot:nth-child(3) {
-        grid-row: 1;
-        grid-column: 1;
-      }
-      .dot:nth-child(4) {
-        grid-row: 1;
-        grid-column: 3;
-      }
-      .dot:nth-child(5) {
-        grid-row: 3;
-        grid-column: 1;
-      }
-      .dot:last-child {
-        grid-row: 2;
-        grid-column: 3;
-      }
+  }
+  .side.top .dot:first-child {
+    grid-row: 1;
+    grid-column: 1;
+  }
+  .side.left .dot:first-child {
+    grid-row: 3;
+    grid-column: 1;
+  }
+  .side.back {
+    .dot:first-child {
+      grid-row: 2;
+      grid-column: 1;
     }
-    .front {
-      transform: rotateY(0deg) translateZ(40px);
+    .dot:nth-child(2) {
+      grid-row: 3;
+      grid-column: 3;
     }
-    .right {
-      transform: rotateY(90deg) translateZ(40px);
+    .dot:nth-child(3) {
+      grid-row: 1;
+      grid-column: 1;
     }
-    .back {
-      transform: rotateY(180deg) translateZ(40px);
+    .dot:nth-child(4) {
+      grid-row: 1;
+      grid-column: 3;
     }
-    .left {
-      transform: rotateY(-90deg) translateZ(40px);
+    .dot:nth-child(5) {
+      grid-row: 3;
+      grid-column: 1;
     }
-    .top {
-      transform: rotateX(90deg) translateZ(40px);
-    }
-    .bottom {
-      transform: rotateX(-90deg) translateZ(40px);
+    .dot:last-child {
+      grid-row: 2;
+      grid-column: 3;
     }
   }
-  .cube.show-1 {
-    animation-name: rotationOne;
-    animation-duration: 4s;
-    animation-fill-mode: forwards;
+  .cover,
+  .inner {
+    background: #e0e0e0;
+    box-shadow: none;
   }
-  .cube.show-2 {
-    animation-name: rotationTwo;
-    animation-duration: 4s;
-    animation-fill-mode: forwards;
-  }
-  .cube.show-3 {
-    animation-name: rotationThree;
-    animation-duration: 4s;
-    animation-fill-mode: forwards;
-  }
-  .cube.show-4 {
-    animation-name: rotationFour;
-    animation-duration: 4s;
-    animation-fill-mode: forwards;
-  }
-  .cube.show-5 {
-    animation-name: rotationFive;
-    animation-duration: 4s;
-    animation-fill-mode: forwards;
-  }
-  .cube.show-6 {
-    animation-name: rotationSix;
-    animation-duration: 4s;
-    animation-fill-mode: forwards;
-  }
-  @keyframes rotationOne {
-    20% {
-      transform: rotateY(0deg) rotateX(-180deg) rotateZ(-140deg);
+  .cover {
+    border-radius: 0;
+    transform: translateZ(0px);
+    &.x {
+      transform: rotateY(90deg);
     }
-    100% {
-      transform: rotateY(720deg) rotateX(360deg) rotateZ(360deg);
+    &.z {
+      transform: rotateX(90deg);
     }
   }
-  @keyframes rotationTwo {
-    60% {
-      transform: rotateY(0deg) rotateX(180deg) rotateZ(-140deg);
-    }
-    100% {
-      transform: rotateY(540deg) rotateX(360deg) rotateZ(360deg);
+  .front {
+    transform: translateZ(40px);
+    &.inner {
+      transform: translateZ(38px);
     }
   }
-  @keyframes rotationThree {
-    20% {
-      transform: rotateY(0deg) rotateX(-180deg) rotateZ(-140deg);
-    }
-    100% {
-      transform: rotateY(-90deg) rotateX(720deg) rotateZ(360deg);
+  .back {
+    transform: rotateX(-180deg) translateZ(40px);
+    &.inner {
+      transform: rotateX(-180deg) translateZ(38px);
     }
   }
-  @keyframes rotationFour {
-    100% {
-      transform: rotateY(90deg) rotateX(360deg) rotateZ(720deg);
+  .right {
+    transform: rotateY(90deg) translateZ(40px);
+    &.inner {
+      transform: rotateY(90deg) translateZ(38px);
     }
   }
-  @keyframes rotationFive {
-    100% {
-      transform: rotateX(-90deg) rotateZ(720deg) rotateY(360deg);
+  .left {
+    transform: rotateY(-90deg) translateZ(40px);
+    &.inner {
+      transform: rotateY(-90deg) translateZ(38px);
     }
   }
-  @keyframes rotationSix {
-    100% {
-      transform: rotateX(90deg) rotateY(-360deg) rotateZ(-720deg);
+  .top {
+    transform: rotateX(90deg) translateZ(40px);
+    &.inner {
+      transform: rotateX(90deg) translateZ(38px);
+    }
+  }
+  .bottom {
+    transform: rotateX(-90deg) translateZ(40px);
+    &.inner {
+      transform: rotateX(-90deg) translateZ(38px);
     }
   }
 `;
+export default class DieRolled extends Component {
+  constructor(props) {
+    super(props);
+    this.die = createRef();
+    this.animation = null;
+    this.reset = null;
+  }
 
-const arrayOfImages = [one, two, three, four, five, six];
+  rollDie = () => {
+    const positions = [
+      {
+        x: 0,
+        y: 0,
+        z: 0
+      },
+      {
+        x: -90,
+        y: 0,
+        z: 0
+      },
+      {
+        x: 0,
+        y: -90,
+        z: 0
+      },
+      {
+        x: 0,
+        y: 90,
+        z: 0
+      },
+      {
+        x: 90,
+        y: -360,
+        z: 360
+      },
+      {
+        x: 0,
+        y: -180,
+        z: 0
+      }
+    ];
 
-export default function DieRolled({ num, isLowest, delay }) {
-  return (
-    <Scene delay={delay}>
-      <div className={`cube show-${num}`}>
-        <div className="face front">
+    this.animation = new TimelineMax();
+    this.animation
+      .to(this.die.current, 0.5, {
+        rotationX: 1080,
+        rotationY: 1080,
+        rotationZ: 1080
+      })
+      .to(
+        this.die.current,
+        2,
+        {
+          rotationX: positions[this.props.num - 1].x,
+          rotationY: positions[this.props.num - 1].y,
+          rotationZ: positions[this.props.num - 1].z
+        },
+        `+=${this.props.delay * 0.98}`
+      );
+  };
+
+  componentDidUpdate(prevProps) {
+    this.rollDie();
+  }
+  componentDidMount() {
+    this.rollDie();
+  }
+
+  makeRandom = (min, max) => {
+    const random = Math.floor(Math.random() * (max - min + 1) + min);
+    console.log(random);
+    return random;
+  };
+
+  render() {
+    return (
+      <Scene id="dice" ref={this.die}>
+        <div className="side front">
           <div className="dot" />
         </div>
-        <div className="face back">
-          <div className="dot" />
-          <div className="dot" />
+        <div className="side front inner" />
+        <div className="side top">
+          <div className="dot dtop dleft" />
+          <div className="dot dbottom dright" />
         </div>
-        <div className="face right">
-          <div className="dot" />
-          <div className="dot" />
-          <div className="dot" />
+        <div className="side top inner" />
+        <div className="side right">
+          <div className="dot dtop dleft" />
+          <div className="dot center" />
+          <div className="dot dbottom dright" />
         </div>
-        <div className="face left">
-          <div className="dot" />
-          <div className="dot" />
-          <div className="dot" />
-          <div className="dot" />
+        <div className="side right inner" />
+        <div className="side left">
+          <div className="dot dtop dleft" />
+          <div className="dot dtop dright" />
+          <div className="dot dbottom dleft" />
+          <div className="dot dbottom dright" />
         </div>
-        <div className="face top">
-          <div className="dot" />
-          <div className="dot" />
-          <div className="dot" />
-          <div className="dot" />
-          <div className="dot" />
+        <div className="side left inner" />
+        <div className="side bottom">
+          <div className="dot center" />
+          <div className="dot dtop dleft" />
+          <div className="dot dtop dright" />
+          <div className="dot dbottom dleft" />
+          <div className="dot dbottom dright" />
         </div>
-        <div className="face bottom">
-          <div className="dot" />
-          <div className="dot" />
-          <div className="dot" />
-          <div className="dot" />
-          <div className="dot" />
-          <div className="dot" />
+        <div className="side bottom inner" />
+        <div className="side back">
+          <div className="dot dtop dleft" />
+          <div className="dot dtop dright" />
+          <div className="dot dbottom dleft" />
+          <div className="dot dbottom dright" />
+          <div className="dot center dleft" />
+          <div className="dot center dright" />
         </div>
-      </div>
-    </Scene>
-  );
+        <div className="side back inner" />
+        <div className="side cover x" />
+        <div className="side cover y" />
+        <div className="side cover z" />
+      </Scene>
+    );
+  }
 }
