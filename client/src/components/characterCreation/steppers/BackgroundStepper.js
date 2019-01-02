a8172cac88e192ea46385b3c61c15166a5c3c6d3
Anthony Agnone <ada60@pitt.edu>
2019-01-02 06:59:15 -0700

    Commented out or deleted unused variables and imports

--- client/src/components/characterCreation/steppers/BackgroundStepper.js
+++ client/src/components/characterCreation/steppers/BackgroundStepper.js
@@ -1,63 +1,53 @@
-import React, { Component } from 'react'
-import CustomStepper from './StepperBase'
-import TabPanel from '../TabPanel'
-import axios from 'axios'
-import PromiseHandler from '../../api/PromiseHandler'
-import { withinView } from '../../api/View'
+import React, { Component } from 'react';
+import CustomStepper from './StepperBase';
 
-import GenderList from '../../lists/GenderList'
-import AlignmentList from '../../lists/AlignmentList'
-import { withCharacter } from '../../../contexts/CharacterContext'
-
-const cors = 'https://vschool-cors.herokuapp.com/?url='
+import GenderList from '../../lists/GenderList';
+import AlignmentList from '../../lists/AlignmentList';
+import { withCharacter } from '../../../contexts/CharacterContext';
 
 class BackgroundStepper extends Component {
-  constructor(props) {
-    super(props)
+	constructor(props) {
+		super(props);
 
-    this.state = {}
-  }
+		this.state = {};
+	}
 
-  /*
-   *
-   * AXIOS FUNCTIONS
-   *
-   */
+	/*
+	 *
+	 * CHARACTER BUILDER FUNCTIONS
+	 *
+	 */
 
-  /*
-   *
-   * CHARACTER BUILDER FUNCTIONS
-   *
-   */
+	handleSelected = (category, value) => {
+		this.props.setValue(category, value);
+	};
 
-  handleSelected = (category, value) => {
-    this.props.setValue(category, value)
-  }
+	render() {
+		//Array of Info
+		const headerArr = [
+			'Choose Your Gender',
+			'Choose Your Alignment',
+			'Something Else Here',
+			'Something Else Here Again'
+		];
 
-  render() {
-    //Array of Hader Info
-    const headerArr = [
-      'Choose Your Gender',
-      'Choose Your Alignment',
-      'Something Else Here',
-      'Something Else Here Again'
-    ]
+		// Array of StepContent to put in Stepper
+		const contentArr = [
+			// Gender List
+			<GenderList handleSelected={this.handleSelected} />,
+			// Alignment
+			<AlignmentList handleSelected={this.handleSelected} />,
+			// dont know yet
+			<div />,
+			// dont know yet
+			<div />
+		];
 
-    // Array of StepContent to put in Stepper
-    const contentArr = [
-      // Gender List
-      <GenderList handleSelected={this.handleSelected} />,
-      // Alignment
-      <AlignmentList handleSelected={this.handleSelected} />,
-      // dont know yet
-      <div />,
-      // dont know yet
-      <div />
-    ]
-
-    return <CustomStepper className="background" headerArr={headerArr} contentArr={contentArr} />
-    //return <TabPanel />;
-  }
+		return (
+			<CustomStepper className="background" headerArr={headerArr} contentArr={contentArr} />
+		);
+		//return <TabPanel />;
+	}
 }
 
-export default withCharacter(BackgroundStepper)
+export default withCharacter(BackgroundStepper);
--- client/src/components/characterCreation/steppers/StepperBase.js
+++ client/src/components/characterCreation/steppers/StepperBase.js
@@ -1,10 +1,9 @@
-import React, { Component } from 'react'
-import styled from 'styled-components'
-import StepHeader from './stepperComponents/StepHeader'
-import StepContent from './stepperComponents/StepContent'
-import { withCharacter } from '../../../contexts/CharacterContext'
-import bg from '../assets/darkBg.png'
-import { lighten } from 'polished'
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import StepHeader from './stepperComponents/StepHeader';
+import StepContent from './stepperComponents/StepContent';
+import { withCharacter } from '../../../contexts/CharacterContext';
+import bg from '../assets/darkBg.png';
 
 const Wrapper = styled.div`
   width: 60vw;
@@ -15,13 +14,13 @@ const Wrapper = styled.div`
   transform: translate(0,0);
   background-color: #221e1f;
   background-image: url('${bg}');
-`
+`;
 //header of active section should be 2em . 2.2em
 const Step = styled.div`
-  padding: 0 20px 24px 50px;
-  position: relative;
-  transition: all 0.4s ease-in-out;
-  ${'' /* &::after {
+	padding: 0 20px 24px 50px;
+	position: relative;
+	transition: all 0.4s ease-in-out;
+	${'' /* &::after {
     content: '';
     position: absolute;
     height: 100%;
@@ -32,126 +31,126 @@ const Step = styled.div`
     top: 0;
     z-index: 1;
   } */}
-  &.minimized {
-    transition: background-color 0.3s ease-in-out;
-    cursor: pointer;
-    :hover {
-      background-color: rgba(0, 0, 0, 0.06);
-    }
-    .step-content {
-      height: 0;
-      padding: 0;
-    }
-  }
-`
+	&.minimized {
+		transition: background-color 0.3s ease-in-out;
+		cursor: pointer;
+		:hover {
+			background-color: rgba(0, 0, 0, 0.06);
+		}
+		.step-content {
+			height: 0;
+			padding: 0;
+		}
+	}
+`;
 
 const ButtonContainer = styled.div`
-  width: 33%;
-  margin: 0 auto;
-  display: flex;
-  justify-content: space-around;
-  button {
-    border: 0;
-    padding: 10px 20px;
-    border-radius: 4px;
-    background-color: #7f7174;
-    box-shadow: 0 5px 10px -3px rgba(99, 99, 0, 0.3);
-    color: #d9e1be;
-    transition: all 0.3s ease-in-out;
-    cursor: pointer;
-    &:hover {
-      background-color: #570002;
-    }
-    &:focus {
-      outline: 0;
-    }
-  }
-  .next-button {
-    background-color: #570002;
-    color: #d9e1be;
-    &:hover {
-      background-color: #d9e1be;
-      color: #570002;
-    }
-  }
-`
+	width: 33%;
+	margin: 0 auto;
+	display: flex;
+	justify-content: space-around;
+	button {
+		border: 0;
+		padding: 10px 20px;
+		border-radius: 4px;
+		background-color: #7f7174;
+		box-shadow: 0 5px 10px -3px rgba(99, 99, 0, 0.3);
+		color: #d9e1be;
+		transition: all 0.3s ease-in-out;
+		cursor: pointer;
+		&:hover {
+			background-color: #570002;
+		}
+		&:focus {
+			outline: 0;
+		}
+	}
+	.next-button {
+		background-color: #570002;
+		color: #d9e1be;
+		&:hover {
+			background-color: #d9e1be;
+			color: #570002;
+		}
+	}
+`;
 
 class StepperBase extends Component {
-  constructor(props) {
-    super(props)
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      step: 0
-    }
-  }
+		this.state = {
+			step: 0
+		};
+	}
 
-  /*
-   *
-   * STEPPER FUNCTIONS
-   *
-   */
+	/*
+	 *
+	 * STEPPER FUNCTIONS
+	 *
+	 */
 
-  handleActiveStep = step => {
-    if (step === this.state.step) return 'step'
-    else return 'step minimized'
-  }
+	handleActiveStep = step => {
+		if (step === this.state.step) return 'step';
+		else return 'step minimized';
+	};
 
-  handleNextStep = () => {
-    this.setState(
-      prevState => ({
-        step: (prevState.step -= 100)
-      }),
-      () => {
-        setTimeout(() => {
-          this.setState(prevState => ({
-            step: (prevState.step += 101)
-          }))
-        }, 400)
-      }
-    )
-  }
+	handleNextStep = () => {
+		this.setState(
+			prevState => ({
+				step: prevState.step -= 100
+			}),
+			() => {
+				setTimeout(() => {
+					this.setState(prevState => ({
+						step: prevState.step += 101
+					}));
+				}, 400);
+			}
+		);
+	};
 
-  handleLastStep = () => {
-    if (this.state.step !== 0) {
-      this.setState(
-        prevState => ({
-          step: (prevState.step -= 100)
-        }),
-        () => {
-          setTimeout(() => {
-            this.setState(prevState => ({
-              step: (prevState.step += 99)
-            }))
-          }, 400)
-        }
-      )
-    }
-  }
+	handleLastStep = () => {
+		if (this.state.step !== 0) {
+			this.setState(
+				prevState => ({
+					step: prevState.step -= 100
+				}),
+				() => {
+					setTimeout(() => {
+						this.setState(prevState => ({
+							step: prevState.step += 99
+						}));
+					}, 400);
+				}
+			);
+		}
+	};
 
-  render() {
-    let stepArr = []
-    for (let i = 0; i < this.props.headerArr.length; i++) {
-      stepArr.push(
-        <Step className={this.state.step === i ? 'step' : 'step minimized'}>
-          <StepHeader active={this.state.step === i}>{this.props.headerArr[i]}</StepHeader>
-          <StepContent>{this.props.contentArr[i]}</StepContent>
-        </Step>
-      )
-    }
-    return (
-      <Wrapper>
-        {stepArr}
-        <ButtonContainer className="buttons">
-          <button className="back-btn" onClick={() => this.handleLastStep()}>
-            back
-          </button>
-          <button className="next-button" onClick={() => this.handleNextStep()}>
-            Next
-          </button>
-        </ButtonContainer>
-      </Wrapper>
-    )
-  }
+	render() {
+		let stepArr = [];
+		for (let i = 0; i < this.props.headerArr.length; i++) {
+			stepArr.push(
+				<Step className={this.state.step === i ? 'step' : 'step minimized'}>
+					<StepHeader active={this.state.step === i}>{this.props.headerArr[i]}</StepHeader>
+					<StepContent>{this.props.contentArr[i]}</StepContent>
+				</Step>
+			);
+		}
+		return (
+			<Wrapper>
+				{stepArr}
+				<ButtonContainer className="buttons">
+					<button className="back-btn" onClick={() => this.handleLastStep()}>
+						back
+					</button>
+					<button className="next-button" onClick={() => this.handleNextStep()}>
+						Next
+					</button>
+				</ButtonContainer>
+			</Wrapper>
+		);
+	}
 }
 
-export default withCharacter(StepperBase)
+export default withCharacter(StepperBase);
